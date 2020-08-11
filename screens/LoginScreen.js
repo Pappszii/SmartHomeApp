8f004923263064c2b73e58740bfcd5909229663d
Pappszii <erdelyidanii@gmail.com>
2020-08-11 21:37:18 +0200

    Implemented authentication to firebase

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,56 +1,71 @@
-import React from "react"
-import {StyleSheet,Text,View,Alert,TextInput,TouchableOpacity} from "react-native";
-import * as firebase from 'firebase'
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import * as firebase from "firebase";
 
 export default class LoginScreen extends React.Component {
-   state ={
-    email:"",
-    password="",
-    errorMessage:null
-     }
-   
-   
-   
-    render() {
-        return (
-          <View style={styles.container}>
-            <Text style={styles.greeting}>
-              {"Hello again.\n Welcome back."}
-            </Text>
+  state = {
+    email: "",
+    password: "",
+    errorMessage: null,
+  };
 
-            <View style={styles.errorMessage}>
-              <Text>Error</Text>
-            </View>
+  handleLogin=() =>{
+      const{email,password} = this.state
 
-            <View style={styles.form}>
-              <View>
-                <Text style={styles.inputTitle}>Email Address</Text>
-                <TextInput
-                  style={styles.input}
-                  autoCapitalize="none"
-                ></TextInput>
-              </View>
+      firebase.auth().signInWithEmailAndPassword(email,password).catch(error=> this.setState({errorMessage:error.message}))
+  }
 
-              <View style={{ marginTop: 32 }}>
-                <Text style={styles.inputTitle}>Password</Text>
-                <TextInput
-                  style={styles.input}
-                  autoCapitalize="none"
-                  secureTextEntry
-                ></TextInput>
-              </View>
-            </View>
-            <TouchableOpacity style={styles.button}>
-              <Text style={{ color: "#FFF", fontWeight: "500" }}>Sign in</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{alignSelf:"center",marginTop:32}}>
-              <Text style={{ color: "#414959", fontSize: 13 }}>
-                  New to SmartHomeApp? <Text style={{color:"#E9446A"}}>Sign Up</Text>
-              </Text>
-            </TouchableOpacity>
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.greeting}>{"Hello again.\n Welcome back."}</Text>
+
+        <View style={styles.errorMessage}>
+          {this.state.errorMessage && (
+            <Text style={styles.error}>{this.state.errorMessage}</Text>
+          )}
+        </View>
+        <View style={styles.form}>
+          <View>
+            <Text style={styles.inputTitle}>Email Address</Text>
+            <TextInput
+              style={styles.input}
+              autoCapitalize="none"
+              onChangeText={(email) => this.setState({ email })}
+              value={this.state.email}
+            ></TextInput>
           </View>
-        );
-    }
+
+          <View style={{ marginTop: 32 }}>
+            <Text style={styles.inputTitle}>Password</Text>
+            <TextInput
+              style={styles.input}
+              autoCapitalize="none"
+              onChangeText={(password) => this.setState({ password })}
+              value={this.state.password}
+              secureTextEntry
+            ></TextInput>
+          </View>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={this.handleLogin}>
+          <Text style={{ color: "#FFF", fontWeight: "500" }}>Sign in</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{ alignSelf: "center", marginTop: 32 }}>
+          <Text style={{ color: "#414959", fontSize: 13 }}>
+            New to SmartHomeApp?{" "}
+            <Text style={{ color: "#E9446A" }}>Sign Up</Text>
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -71,6 +86,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginHorizontal: 33,
   },
+  error: {
+    color: "#E9446A",
+    fontSize: 13,
+    textAlign: "center",
+    fontWeight: "600",
+  },
   inputTitle: {
     color: "#8A8F9E",
     fontSize: 18,
@@ -83,12 +104,12 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: "rgb(138,143,158)",
   },
-  button:{
-      marginHorizontal:30,
-      backgroundColor:'#E9446A',
-      borderRadius:4,
-      height:52,
-      alignItems:"center",
-      justifyContent:"center"
-  }
+  button: {
+    marginHorizontal: 30,
+    backgroundColor: "#E9446A",
+    borderRadius: 4,
+    height: 52,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
