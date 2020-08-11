edafbe34d67089183145770454c76c7ed8f53746
Pappszii <erdelyidanii@gmail.com>
2020-08-11 21:24:58 +0200

    LoginScreen form created

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 import * as firebase from 'firebase'
+import { TextInput } from "react-native-gesture-handler";
 
 export default class LoadingScreen extends React.Component {
+    componentDidMount(){
+        firebase.auth().onAuthStateChanged(user=>{
+            this.props.navigation.navigate(user?"App":"Auth");
+        });
+    }
+
     render() {
         return (
             <View style={styles.container}>
-               <Text>Loading...ZSAMO</Text>
+               <Text style={styles.greeting}>Loading...</Text>
                 <ActivityIndicator size="large"></ActivityIndicator>
             </View>
         );
@@ -14,15 +21,33 @@ export default class LoadingScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent:"center",
-        alignItems:"center"
-    },
-    greeting:{
-        marginTop:32,
-        fontSize:18,
-        fontWeight:"400",
-        textAlign:"center"
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  greeting: {
+    marginTop: 32,
+    fontSize: 18,
+    fontWeight: "400",
+    textAlign: "center",
+  },
+  errorMessage: {
+    height: 72,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 33,
+  },
+  inputTitle: {
+    color: "#8A8F9E",
+    fontSize: 18,
+    textTransform: "uppercase",
+  },
+  input: {
+    borderBottomColor: "#8A8F9E",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    height: 40,
+    fontSize: 15,
+    color: 'rgb(138,143,158)',
+  },
 });
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,40 +1,94 @@
 import React from "react"
-import {view,Text,StyleSheet} from "react-native"
-
+import {StyleSheet,Text,View,Alert,TextInput,TouchableOpacity} from "react-native";
+import * as firebase from 'firebase'
 
 export default class LoginScreen extends React.Component {
+   state ={
+    email:"",
+    password="",
+    errorMessage:null
+     }
+   
+   
+   
     render() {
         return (
-            <View style={styles.container}>
-                <text style={styles.greeting}>{'Hello again.\n Welcome back.'}</text>
+          <View style={styles.container}>
+            <Text style={styles.greeting}>
+              {"Hello again.\n Welcome back."}
+            </Text>
 
-                <View style={styles.errorMessage}>
-                    <text>Error</text>
-                </View>
-
-                <View style={styles.form}>
-                    <text style ={styles.inputTitle}>Email Address</text>
-                    <TextInput style ={styles.input} autoCapitalize="none"></TextInput>
-                </View>
+            <View style={styles.errorMessage}>
+              <Text>Error</Text>
             </View>
+
+            <View style={styles.form}>
+              <View>
+                <Text style={styles.inputTitle}>Email Address</Text>
+                <TextInput
+                  style={styles.input}
+                  autoCapitalize="none"
+                ></TextInput>
+              </View>
+
+              <View style={{ marginTop: 32 }}>
+                <Text style={styles.inputTitle}>Password</Text>
+                <TextInput
+                  style={styles.input}
+                  autoCapitalize="none"
+                  secureTextEntry
+                ></TextInput>
+              </View>
+            </View>
+            <TouchableOpacity style={styles.button}>
+              <Text style={{ color: "#FFF", fontWeight: "500" }}>Sign in</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{alignSelf:"center",marginTop:32}}>
+              <Text style={{ color: "#414959", fontSize: 13 }}>
+                  New to SmartHomeApp? <Text style={{color:"#E9446A"}}>Sign Up</Text>
+              </Text>
+            </TouchableOpacity>
+          </View>
         );
     }
 }
 
-const styles= StyleSheet.create({
-    container:{
-        flex:1,
-    },
-    greeting:{
-        marginTop:32,
-        fontSize:18,
-        fontWeight:"400",
-        textAlign:"center"
-    },
-    errorMessage:{
-        height:72,
-        alignItems:"center",
-        justifyContent:"center",
-        marginHorizontal:30
-    }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  greeting: {
+    marginTop: 32,
+    fontSize: 18,
+    fontWeight: "400",
+    textAlign: "center",
+  },
+  errorMessage: {
+    height: 72,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 33,
+  },
+  inputTitle: {
+    color: "#8A8F9E",
+    fontSize: 18,
+    textTransform: "uppercase",
+  },
+  input: {
+    borderBottomColor: "#8A8F9E",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    height: 40,
+    fontSize: 15,
+    color: "rgb(138,143,158)",
+  },
+  button:{
+      marginHorizontal:30,
+      backgroundColor:'#E9446A',
+      borderRadius:4,
+      height:52,
+      alignItems:"center",
+      justifyContent:"center"
+  }
 });
